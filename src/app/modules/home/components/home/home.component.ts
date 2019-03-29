2ee36f1f75d3d4fcf812ccd3395e01c463d53242
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-03-29 12:12:40 -0400

    Changed location of copy service

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
-import { CopyService } from 'src/app/services/copy/copy.service';
+import { CopyService } from '../../services/copy/copy.service';
 
 @Component({
   selector: 'app-home-home',
